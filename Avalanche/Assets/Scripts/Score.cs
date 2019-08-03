232a0fb97a35cef0d86d9b060deec6cd0f41c174
James <Jamesmerrifield112@gmail.com>
2019-08-03 17:36:48 +1000

    Scripts
    Manager/PlayerCollision/Score

--- Avalanche/Assets/Scripts/Score.cs
+++ Avalanche/Assets/Scripts/Score.cs
@@ -6,11 +6,32 @@ public class Score : MonoBehaviour
 {
     public Transform player;
     public Text scoreText;
+    public Text hiscore;
+    public int x = 0;
+    public int y = 0;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        scoreText.text = player.position.y.ToString("0");
-        
+        hiscore.text = PlayerPrefs.GetInt("Hiscore", 0).ToString();
+    }
+    
+    public void Update()
+    {
+
+        scoreText.text =  player.position.y.ToString("0");
+        int.TryParse(scoreText.text, out x);
+
+        int number = x;
+        if (number > PlayerPrefs.GetInt("Hiscore", 0))
+        {
+            PlayerPrefs.SetInt("Hiscore", number);
+            hiscore.text = number.ToString();
+        }
+
+    }
+    public void Reset()
+    {
+        PlayerPrefs.DeleteAll();
+    
     }
 }
